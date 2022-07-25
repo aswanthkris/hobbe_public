f8b84a18a3a7a27531ba540cf2b7295cc2fd0a43
aswanthkris <109384688+aswanthkris@users.noreply.github.com>
2022-07-25 20:36:47 +0530

    Add files via upload

---
+++ Public_Websites--main/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#submit-form").validate({
+        rules:{
+            first_name:{
+                required:true,
+                minlength:4
+            },
+            last_name:{
+                required:true,
+                minlength:4
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            password:{
+                required:true,
+                minlength:6
+            }
+        }
+        
+    })
+})
+
+$(document).ready(function(){
+    $("#login-form").validate({
+        rules:{
+            user_name:{
+                required:true,
+                minlength:4
+            },
+            email:{
+                required:true,
+                email:true
+            }
+        }
+    })
+})
